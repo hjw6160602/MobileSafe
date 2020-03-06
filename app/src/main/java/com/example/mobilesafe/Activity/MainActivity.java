3ff03ac95a2b7c97e47ed9acc9f399e657126f52
贺嘉炜 <hejiawei@hejiaweideMacBook-Pro.local>
2020-03-06 19:54:24 +0800

    first commit

---
+++ app/src/main/java/com/example/mobilesafe/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mobilesafe.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobilesafe.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
